a179217edb3fb38afcfcdc579fc752ceafab73fa
ton-ayr <ayrtoncbarreira@gmail.com>
2024-09-17 17:41:18 -0300

    logica inicial para adicionar no carrinho

---
+++ js/app.js
@@ -0,0 +1,15 @@
+function adicionar() {
+    let produto = document.getElementById('produto').value;
+    let nomeProduto = produto.split('-')[0];
+    let valorUnitario = produto.split('R$')[1];
+    let quantidade = document.getElementById('quantidade').value;
+    let preco = quantidade * valorUnitario;
+    let carrinho = document.getElementById('lista-produtos');
+    carrinho.innerHTML = `<section class="carrinho__produtos__produto">
+          <span class="texto-azul">${quantidade}x</span> ${nomeProduto} <span class="texto-azul">R$${preco}</span>
+        </section>`;
+}
+
+function limpar() {
+    
+}
